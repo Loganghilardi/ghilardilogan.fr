f465681ef8f09b86d7618eed4d7ead8207b2524b
Logan <logan.ghilardi@netways.fr>
2020-11-10 11:56:42 +0100

    tmp

--- ghilardilogan.fr/index.php
+++ ghilardilogan.fr/index.php
@@ -1,6 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Logan Ghilardi</title>
+</head>
+<body>
 <h1>Bienvenue sur mon site</h1>
 <?php
 date_default_timezone_set('Europe/Paris');
 echo 'Nous sommes le ' . date('d-m-Y');
 ?>
-
+</body>
+</html>
