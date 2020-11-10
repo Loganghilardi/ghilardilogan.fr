1cb03ae15e1ecd59999073a974914b6c91b5b2f0
Logan <logan.ghilardi@netways.fr>
2020-11-10 12:41:14 +0100

    test

--- ghilardilogan.fr/index.php
+++ ghilardilogan.fr/index.php
@@ -8,7 +8,7 @@
 <h5>Bienvenue sur mon site</h5>
 <?php
 date_default_timezone_set('Europe/Paris');
-echo 'Nous sommes le ' . date('d-m-Y');
+echo 'Nous sommes le ' . date('d-m-Y') . 'Il est ' . date('H:i:s');
 ?>
 </body>
 </html>
