5f691fb9ad6a7d4d9a1ac7d49301b445b70009a8
Logan <logan.ghilardi@netways.fr>
2020-11-10 13:00:18 +0100

    ajoutBootstrap

--- ghilardilogan.fr/index.php
+++ ghilardilogan.fr/index.php
@@ -3,13 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Logan Ghilardi</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
-<h5>Bienvenue sur mon site</h5>
-<?php
-date_default_timezone_set('Europe/Paris');
-echo 'Nous sommes le ' . date('d-m-Y') . ' Il est ' . date('H:i:s');
-?>
-<p>Comment ça va ?</p>
+<div class="container-flex">
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </nav>
+    </header>
+    <h5>Bienvenue sur mon site</h5>
+    <?php
+    date_default_timezone_set('Europe/Paris');
+    echo 'Nous sommes le ' . date('d-m-Y') . ' Il est ' . date('H:i:s');
+    ?>
+    <p>Comment ça va ?</p>
+</div>
 </body>
 </html>
